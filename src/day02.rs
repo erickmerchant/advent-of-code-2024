94a7b181c5dc4523b5b5319b8e3b8e7787daa37e
erickmerchant <erickmerchant@gmail.com>
2024-12-02 01:44:06 -0500

    Day 2

---
+++ src/day02.rs
@@ -0,0 +1,114 @@
+// use std::collections::HashMap;
+
+fn test_parts(parts: Vec<isize>) -> usize {
+    let mut parts = parts.clone();
+
+    if !parts.is_sorted() {
+        parts.reverse();
+
+        if !parts.is_sorted() {
+            return 0;
+        }
+    }
+
+    let mut prev = None;
+
+    for part in parts {
+        match prev {
+            None => {
+                prev = Some(part);
+            }
+            Some(prev_part) => {
+                let diff = (prev_part - part).abs();
+
+                if diff < 1 || diff > 3 {
+                    return 0;
+                }
+
+                prev = Some(part);
+            }
+        }
+    }
+
+    1
+}
+
+pub fn part1(input: Vec<String>) -> usize {
+    let mut result = 0;
+
+    for line in input {
+        let parts: Vec<isize> = line
+            .split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect();
+
+        result += test_parts(parts);
+    }
+
+    result
+}
+
+pub fn part2(input: Vec<String>) -> usize {
+    let mut result = 0;
+
+    'outer: for line in input {
+        let parts: Vec<isize> = line
+            .split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect();
+
+        let r = test_parts(parts.clone());
+
+        result += r;
+
+        if r == 1 {
+            continue 'outer;
+        }
+
+        for i in 0..parts.len() {
+            let mut parts = parts.clone();
+            parts.remove(i);
+
+            let r = test_parts(parts);
+
+            result += r;
+
+            if r == 1 {
+                continue 'outer;
+            }
+        }
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn get_fixture() -> Vec<String> {
+        "7 6 4 2 1
+        1 2 7 8 9
+        9 7 6 2 1
+        1 3 2 4 5
+        8 6 4 4 1
+        1 3 6 7 9"
+            .split('\n')
+            .map(|s| s.trim().to_string())
+            .collect()
+    }
+
+    #[test]
+    fn test_part1() {
+        let fixture = get_fixture();
+
+        assert_eq!(part1(fixture), 2);
+    }
+
+    #[test]
+    fn test_part2() {
+        let fixture = get_fixture();
+
+        assert_eq!(part2(fixture), 4);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 pub mod day01;
+pub mod day02;
 
 use clap::Parser;
 use core::panic;
@@ -21,6 +22,8 @@ fn main() {
     let output = match (args.day, args.part) {
         (1, 1) => day01::part1(input),
         (1, 2) => day01::part2(input),
+        (2, 1) => day02::part1(input),
+        (2, 2) => day02::part2(input),
         _ => panic!("Incomplete day or part"),
     };
 
