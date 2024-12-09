c4d464c830a6f05b7cf1070fcef9fa1794b1da7f
erickmerchant <erickmerchant@gmail.com>
2024-12-09 11:37:10 -0500

    Day 9

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -52,53 +52,89 @@ fn part1(input: String) -> usize {
     total
 }
 
+#[derive(Debug)]
+enum Block {
+    Used(usize),
+    Free(usize),
+}
+
 fn part2(input: String) -> usize {
     let input = input
         .chars()
         .map(|c| c.to_digit(10).unwrap() as usize)
         .collect::<Vec<usize>>();
-    let mut result: Vec<Option<usize>> = vec![];
+    let mut result: Vec<Block> = vec![];
     let mut is_on = true;
-    let mut to_move_count = 0;
     let mut id = 0;
+    let mut free_map: Vec<(usize, usize)> = vec![];
+    let mut used_map: Vec<(usize, usize)> = vec![];
 
-    for d in input {
+    for count in input {
         if is_on {
-            for _ in 0..d {
-                result.push(Some(id));
+            for _ in 0..count {
+                result.push(Block::Used(id));
             }
+
+            used_map.push((id, count));
 
             id += 1;
         } else {
-            for _ in 0..d {
-                result.push(None);
+            for _ in 0..count {
+                result.push(Block::Free(id));
             }
 
-            to_move_count += d;
+            free_map.push((id, count));
         }
 
         is_on = !is_on;
     }
 
-    loop {
-        if to_move_count == 0 {
-            break;
-        }
+    for (id, count) in used_map.iter().rev() {
+        if let Some(index_of_free) = free_map.iter().position(|(_, c)| c >= count) {
+            let free = free_map.get_mut(index_of_free).unwrap();
 
-        if let (Some(a), Some(b)) = (
-            result.iter().position(|i| i.is_none()),
-            result.iter().rposition(|i| i.is_some()),
-        ) {
-            result.swap(a, b)
-        }
+            let free_id = free.0;
+            let mut to_move_count = *count;
 
-        to_move_count -= 1;
+            free.1 -= count;
+
+            loop {
+                if to_move_count == 0 {
+                    break;
+                }
+
+                if let (Some(a), Some(b)) = (
+                    result.iter().position(|r| {
+                        if let Block::Free(i) = r {
+                            *i == free_id
+                        } else {
+                            false
+                        }
+                    }),
+                    result.iter().rposition(|r| {
+                        if let Block::Used(i) = r {
+                            *i == *id
+                        } else {
+                            false
+                        }
+                    }),
+                ) {
+                    if a < b {
+                        result.swap(a, b);
+
+                        to_move_count -= 1;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
     }
 
     let mut total = 0;
 
     for (i, d) in result.iter().enumerate() {
-        if let Some(d) = d {
+        if let Block::Used(d) = d {
             total += d * i;
         }
     }
