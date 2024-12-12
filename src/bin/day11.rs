10d2b2a6a0abf782f48d8d087f972596bafcc5ca
erickmerchant <erickmerchant@gmail.com>
2024-12-11 23:35:14 -0500

    Day 11

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,58 +1,54 @@
 use num::{BigInt, FromPrimitive};
 use rayon::prelude::*;
 
-fn get_result(input: String, iterations: usize) -> usize {
-    let mut stones: Vec<BigInt> = input
-        .split_whitespace()
-        .map(|p| FromPrimitive::from_usize(p.parse::<usize>().unwrap()).unwrap())
-        .collect();
-
-    for _i in 0..iterations {
-        let new_stones: Vec<BigInt> = stones
-            .clone()
-            .par_iter()
-            .map(|stone| {
-                let mut new_stones: Vec<BigInt> = vec![];
-                let stone_as_string = stone.to_string();
-
-                if *stone == BigInt::ZERO {
-                    new_stones.push(FromPrimitive::from_usize(1).unwrap());
-                } else if stone_as_string.len() % 2 == 0 {
-                    let (a, b) = stone_as_string.split_at(stone_as_string.len() / 2);
-
-                    new_stones.push(FromPrimitive::from_usize(a.parse().unwrap()).unwrap());
-                    new_stones.push(FromPrimitive::from_usize(b.parse().unwrap()).unwrap());
-                } else {
-                    new_stones.push(stone * 2024)
-                }
-
-                new_stones
-            })
-            .flatten()
-            .collect();
-
-        stones = new_stones;
+fn get_sum(stones: &[BigInt], iterations: usize) -> usize {
+    if iterations == 0 {
+        return stones.len();
     }
 
-    stones.len()
+    stones
+        .par_iter()
+        .map(|stone| {
+            if *stone == BigInt::ZERO {
+                return get_sum(&[FromPrimitive::from_usize(1).unwrap()], iterations - 1);
+            }
+
+            let stone_as_string = stone.to_str_radix(10);
+
+            if stone_as_string.len() % 2 == 0 {
+                let (a, b) = stone_as_string.split_at(stone_as_string.len() / 2);
+
+                return get_sum(&[a.parse().unwrap(), b.parse().unwrap()], iterations - 1);
+            }
+
+            get_sum(&[stone * 2024], iterations - 1)
+        })
+        .sum()
+}
+
+fn get_result(input: String, iterations: usize) -> usize {
+    let stones: Vec<BigInt> = input
+        .split_whitespace()
+        .map(|p| p.parse().unwrap())
+        .collect();
+
+    get_sum(&stones, iterations)
 }
 
 fn part1(input: String) -> usize {
     get_result(input, 25)
 }
 
-fn part2(_input: String) -> usize {
-    // get_result(input, 75)
-
-    0
-}
+// fn part2(input: String) -> usize {
+//     get_result(input, 75)
+// }
 
 fn main() {
     let input = advent::get_input();
     let input = input.first().unwrap();
 
     println!("{}", part1(input.clone()));
-    println!("{}", part2(input.clone()));
+    // println!("{}", part2(input.clone()));
 }
 
 #[cfg(test)]
