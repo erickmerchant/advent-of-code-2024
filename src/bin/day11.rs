f67c28df2c82b0f8a0e285dd6ece0eeb7601c796
erickmerchant <erickmerchant@gmail.com>
2024-12-20 23:05:46 -0500

    Day 11 - part 2

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,29 +1,34 @@
-use rayon::prelude::*;
+use std::collections::HashMap;
 
-fn get_sum(stones: &[usize], iterations: usize) -> usize {
-    if iterations == 0 {
-        return stones.len();
+fn get_sum(stone: usize, iterations: usize, cache: &mut HashMap<(usize, usize), usize>) -> usize {
+    if let Some(&result) = cache.get(&(stone, iterations)) {
+        return result;
     }
 
-    stones
-        .par_iter()
-        .map(|stone| {
-            if *stone == 0 {
-                return get_sum(&[1], iterations - 1);
-            }
+    let result = {
+        if iterations == 0 {
+            return 1;
+        }
 
-            let length = stone.ilog10() + 1;
+        if stone == 0 {
+            return get_sum(1, iterations - 1, cache);
+        }
 
-            if length % 2 == 0 {
-                let left = stone / 10usize.pow(length / 2);
-                let right = stone % 10usize.pow(length / 2);
+        let length = stone.ilog10() + 1;
 
-                return get_sum(&[left, right], iterations - 1);
-            }
+        if length % 2 == 0 {
+            let left = stone / 10usize.pow(length / 2);
+            let right = stone % 10usize.pow(length / 2);
 
-            get_sum(&[stone * 2024], iterations - 1)
-        })
-        .sum()
+            return get_sum(left, iterations - 1, cache) + get_sum(right, iterations - 1, cache);
+        }
+
+        get_sum(stone * 2024, iterations - 1, cache)
+    };
+
+    cache.insert((stone, iterations), result);
+
+    result
 }
 
 fn get_result(input: String, iterations: usize) -> usize {
@@ -32,11 +37,20 @@ fn get_result(input: String, iterations: usize) -> usize {
         .map(|p| p.parse().unwrap())
         .collect();
 
-    get_sum(&stones, iterations)
+    let mut cache: HashMap<(usize, usize), usize> = HashMap::new();
+
+    stones
+        .iter()
+        .map(|stone| get_sum(*stone, iterations, &mut cache))
+        .sum()
 }
 
 fn part1(input: String) -> usize {
     get_result(input, 25)
+}
+
+fn part2(input: String) -> usize {
+    get_result(input, 75)
 }
 
 fn main() {
@@ -44,6 +58,7 @@ fn main() {
     let input = input.first().unwrap();
 
     println!("{}", part1(input.clone()));
+    println!("{}", part2(input.clone()));
 }
 
 #[cfg(test)]
